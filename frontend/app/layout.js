6f56cfae1e0b2d1ac9da2b5db87e5afe21c1fa3d
Syed Ali Faraz <alii.exe3@gmail.com>
2026-09-05 09:02:05 +0500

    fix: add suppressHydrationWarning to body element in RootLayout

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${fraunces.variable} ${workSans.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>{children}</body>
     </html>
   );
 }
